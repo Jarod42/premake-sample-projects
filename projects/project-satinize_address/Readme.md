6645df3aabb00c71e66ce958b65717d4e5e66b3e
Jarod42 <Jarod42@users.noreply.github.com>
2023-01-24 23:25:28 +0100

    Add project to test `satinize {"address"}` (more for toolset than IDE).

---
+++ projects/project-satinize_address/Readme.md
@@ -0,0 +1,5 @@
+## project-satinize_address:
+
+Test [`satinize {"address"}`](https://premake.github.io/docs/satinize) (compilation only)
+
+Test for toolset gcc/clang/msc and visual studio
---
+++ projects/project-satinize_address/premake5.lua
@@ -0,0 +1,23 @@
+local Root = path.getabsolute(".")
+
+if (_ACTION == nil) then
+	return
+end
+
+local LocationDir = path.join(Root, "solution/", _ACTION)
+
+workspace "Project"
+	location(LocationDir)
+	configurations {"Release"}
+
+	objdir(path.join(LocationDir, "obj")) -- premake adds $(configName)/$(AppName)
+	targetdir(path.join(LocationDir, "bin"))
+	targetname("app")
+	startproject "app"
+
+project "app"
+	kind "ConsoleApp"
+
+	files {path.join(Root, "src/main.cpp")}
+
+	sanitize { "Address" }
