563c106151e82a9a783a04955cc8644e203cce4b
Jarod42 <Jarod42@users.noreply.github.com>
2023-02-03 18:54:45 +0100

    Quick fix for missing dll.

--- projects/project-satinize_address/premake5.lua
+++ projects/project-satinize_address/premake5.lua
@@ -5,6 +5,7 @@ if (_ACTION == nil) then
 end
 
 local LocationDir = path.join(Root, "solution/", _ACTION)
+local vs2019_asan_dll_path = "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/Llvm/lib/clang/15.0.1/lib/windows/clang_rt.asan_dynamic-i386.dll"
 
 workspace "Project"
 	location(LocationDir)
@@ -21,3 +22,7 @@ project "app"
 	files {path.join(Root, "src/main.cpp")}
 
 	sanitize { "Address" }
+
+	filter { "action:vs2019" }
+		postbuildcommands { '{COPY} "' .. vs2019_asan_dll_path .. '" ' .. path.join(LocationDir, "bin") }
+
