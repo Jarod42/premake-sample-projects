ed881e1ad48de7195732683a151ed673ed3f5b7f
Jarod42 <Jarod42@users.noreply.github.com>
2023-02-18 11:30:11 +0100

    Robustify way to find and copy dll paths for windows.

--- projects/project-sanitize_address/premake5.lua
+++ projects/project-sanitize_address/premake5.lua
@@ -5,7 +5,6 @@ if (_ACTION == nil) then
 end
 
 local LocationDir = path.join(Root, "solution/", _ACTION)
-local vs2019_asan_dll_path = "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/Llvm/lib/clang/15.0.1/lib/windows/clang_rt.asan_dynamic-i386.dll"
 
 workspace "Project"
 	location(LocationDir)
@@ -23,6 +22,43 @@ project "app"
 
 	sanitize { "Address" }
 
-	filter { "action:vs2019" }
-		postbuildcommands { '{COPY} "' .. vs2019_asan_dll_path .. '" ' .. path.join(LocationDir, "bin") }
+	-- DLL needed with visual
+	print("host:", os.host())
+	print("target:", os.target())
+if os.target() == "windows" then
+	architecture "x86" -- seems required for filter
 
+	-- {2019,2022}/{Community,Enterprise}/.../$clang_version
+	local x86_asan_dll_paths = table.join(
+		os.matchfiles("C:/Program Files/Microsoft Visual Studio/*/*/VC/Tools/Llvm/lib/clang/*/lib/windows/clang_rt.asan_dynamic-i386.dll"),
+		os.matchfiles("C:/Program Files (x86)/Microsoft Visual Studio/*/*/VC/Tools/Llvm/lib/clang/*/lib/windows/clang_rt.asan_dynamic-i386.dll")
+	)
+	print("x86 DLL found:")
+	for _, dll_path in ipairs(x86_asan_dll_paths) do
+		print(dll_path)
+	end
+
+	local x64_asan_dll_paths = table.join(
+		os.matchfiles("C:/Program Files/Microsoft Visual Studio/*/*/VC/Tools/Llvm/x64/lib/clang/*/lib/windows/clang_rt.asan_dynamic-x86_64.dll"),
+		os.matchfiles("C:/Program Files (x86)/Microsoft Visual Studio/*/*/VC/Tools/Llvm/x64/lib/clang/*/lib/windows/clang_rt.asan_dynamic-x86_64.dll")
+	)
+	print("x64 DLL found:")
+	for _, dll_path in ipairs(x64_asan_dll_paths) do
+		print(dll_path)
+	end
+
+	if #x86_asan_dll_paths ~= 0 then
+		filter {"action:vs*", "toolset:clang", "architecture:x86 or architecture:x32"}
+			postbuildcommands { '{COPY} "' .. x86_asan_dll_paths[1] .. '" ' .. path.join(LocationDir, "bin") }
+
+		filter { "toolset:msc*", "architecture:x86 or architecture:x32" }
+			postbuildcommands { '{COPY} "' .. x86_asan_dll_paths[1] .. '" ' .. path.join(LocationDir, "bin") }
+	end
+	if #x64_asan_dll_paths ~= 0 then
+		filter {"action:vs*", "toolset:clang", "architecture:x86_64 or architecture:x64"}
+			postbuildcommands { '{COPY} "' .. x64_asan_dll_paths[1] .. '" ' .. path.join(LocationDir, "bin") }
+
+		filter { "toolset:msc*", "architecture:x86_64 or architecture:x64" }
+			postbuildcommands { '{COPY} "' .. x64_asan_dll_paths[1] .. '" ' .. path.join(LocationDir, "bin") }
+	end
+end
