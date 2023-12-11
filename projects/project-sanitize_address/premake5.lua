f063090626e535ca083480635fcfb6787e03adee
Jarod42 <Jarod42@users.noreply.github.com>
2023-12-11 22:20:42 +0100

    windows-latest changed, and there are now several asan-DLLs. Try to select the last version available hoping it is the right one.

--- projects/project-sanitize_address/premake5.lua
+++ projects/project-sanitize_address/premake5.lua
@@ -28,10 +28,14 @@ project "app"
 if os.target() == "windows" then
 	architecture "x86" -- seems required for filter
 
-	-- {2019,2022}/{Community,Enterprise}/.../$clang_version
+	-- {2019,2022}/{Community,Enterprise}/.../$msvc_version|$clang_version/...
 	local x86_asan_dll_paths = table.join(
+		os.matchfiles("C:/Program Files (x86)/Microsoft Visual Studio/*/*/VC/Tools/Llvm/lib/clang/*/lib/windows/clang_rt.asan_dynamic-i386.dll"),
+		os.matchfiles("C:/Program Files (x86)/Microsoft Visual Studio/*/*/VC/Tools/MSVC/*/bin/Hostx86/x86/clang_rt.asan_dynamic-i386.dll"),
+		os.matchfiles("C:/Program Files (x86)/Microsoft Visual Studio/*/*/VC/Tools/MSVC/*/bin/Hostx64/x86/clang_rt.asan_dynamic-i386.dll"),
 		os.matchfiles("C:/Program Files/Microsoft Visual Studio/*/*/VC/Tools/Llvm/lib/clang/*/lib/windows/clang_rt.asan_dynamic-i386.dll"),
-		os.matchfiles("C:/Program Files (x86)/Microsoft Visual Studio/*/*/VC/Tools/Llvm/lib/clang/*/lib/windows/clang_rt.asan_dynamic-i386.dll")
+		os.matchfiles("C:/Program Files/Microsoft Visual Studio/*/*/VC/Tools/MSVC/*/bin/Hostx86/x86/clang_rt.asan_dynamic-i386.dll"),
+		os.matchfiles("C:/Program Files/Microsoft Visual Studio/*/*/VC/Tools/MSVC/*/bin/Hostx64/x86/clang_rt.asan_dynamic-i386.dll")
 	)
 	print("x86 DLL found:")
 	for _, dll_path in ipairs(x86_asan_dll_paths) do
@@ -39,8 +43,12 @@ if os.target() == "windows" then
 	end
 
 	local x64_asan_dll_paths = table.join(
+		os.matchfiles("C:/Program Files (x86)/Microsoft Visual Studio/*/*/VC/Tools/Llvm/x64/lib/clang/*/lib/windows/clang_rt.asan_dynamic-x86_64.dll"),
+		os.matchfiles("C:/Program Files (x86)/Microsoft Visual Studio/*/*/VC/Tools/MSVC/*/bin/Hostx86/x64/clang_rt.asan_dynamic-x86_64.dll"),
+		os.matchfiles("C:/Program Files (x86)/Microsoft Visual Studio/*/*/VC/Tools/MSVC/*/bin/Hostx64/x64/clang_rt.asan_dynamic-x86_64.dll"),
 		os.matchfiles("C:/Program Files/Microsoft Visual Studio/*/*/VC/Tools/Llvm/x64/lib/clang/*/lib/windows/clang_rt.asan_dynamic-x86_64.dll"),
-		os.matchfiles("C:/Program Files (x86)/Microsoft Visual Studio/*/*/VC/Tools/Llvm/x64/lib/clang/*/lib/windows/clang_rt.asan_dynamic-x86_64.dll")
+		os.matchfiles("C:/Program Files/Microsoft Visual Studio/*/*/VC/Tools/MSVC/*/bin/Hostx86/x64/clang_rt.asan_dynamic-x86_64.dll"),
+		os.matchfiles("C:/Program Files/Microsoft Visual Studio/*/*/VC/Tools/MSVC/*/bin/Hostx64/x64/clang_rt.asan_dynamic-x86_64.dll")
 	)
 	print("x64 DLL found:")
 	for _, dll_path in ipairs(x64_asan_dll_paths) do
@@ -49,16 +57,16 @@ if os.target() == "windows" then
 
 	if #x86_asan_dll_paths ~= 0 then
 		filter {"action:vs*", "toolset:clang", "architecture:x86 or architecture:x32"}
-			postbuildcommands { '{COPY} "' .. x86_asan_dll_paths[1] .. '" ' .. path.join(LocationDir, "bin/%{cfg.buildcfg}") }
+			postbuildcommands { '{COPYFILE} "' .. x86_asan_dll_paths[#x86_asan_dll_paths] .. '" %[%{!cfg.targetdir}]' }
 
 		filter { "toolset:msc*", "architecture:x86 or architecture:x32" }
-			postbuildcommands { '{COPY} "' .. x86_asan_dll_paths[1] .. '" ' .. path.join(LocationDir, "bin/%{cfg.buildcfg}") }
+			postbuildcommands { '{COPYFILE} "' .. x86_asan_dll_paths[#x86_asan_dll_paths] .. '" %[%{!cfg.targetdir}]' }
 	end
 	if #x64_asan_dll_paths ~= 0 then
 		filter {"action:vs*", "toolset:clang", "architecture:x86_64 or architecture:x64"}
-			postbuildcommands { '{COPY} "' .. x64_asan_dll_paths[1] .. '" ' .. path.join(LocationDir, "bin/%{cfg.buildcfg}") }
+			postbuildcommands { '{COPYFILE} "' .. x64_asan_dll_paths[#x64_asan_dll_paths] .. '" %[%{!cfg.targetdir}]' }
 
 		filter { "toolset:msc*", "architecture:x86_64 or architecture:x64" }
-			postbuildcommands { '{COPY} "' .. x64_asan_dll_paths[1] .. '" ' .. path.join(LocationDir, "bin/%{cfg.buildcfg}") }
+			postbuildcommands { '{COPYFILE} "' .. x64_asan_dll_paths[#x64_asan_dll_paths] .. '" %[%{!cfg.targetdir}]' }
 	end
 end
