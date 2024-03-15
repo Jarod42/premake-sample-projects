059e66e3a20b138a6fbdeb9aff308f58776677cf
Jarod42 <Jarod42@users.noreply.github.com>
2024-03-15 21:08:00 +0100

    Print some info in buildaction_copy executable.

--- projects/project-buildaction_copy/src/main.cpp
+++ projects/project-buildaction_copy/src/main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char** argv)
 {
 	std::filesystem::path current_exe(argv[0]);
 	
+	std::cout << "cwd: " << std::filesystem::current_path() << std::endl;
+	std::cout << "argv[0]: " << current_exe << std::endl;
 	if (std::filesystem::exists(current_exe.parent_path() / "file.txt")) {
 		return 0;
 	}
