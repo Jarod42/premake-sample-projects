ade28fce0b748ab54673b94498f1eb7e9a8f4a3a
Jarod42 <Jarod42@users.noreply.github.com>
2026-06-23 10:30:15 +0200

    Hack to fix sdl2.nuget for clang on windows

--- projects/nuget-SDL/premake5.lua
+++ projects/nuget-SDL/premake5.lua
@@ -15,6 +15,10 @@ workspace "Project"
 	filter { "action:vs*" }
 		nuget { "sdl2.nuget:2.32.8", "sdl2.nuget.redist:2.32.8" }
 
+	filter { 'toolset:clang' } -- As of Clang 11, `_m_prefetch` is conflicting with the winnt.h version
+		defines { '__PRFCHWINTRIN_H' }
+	filter {}
+
 project "app"
 	kind "ConsoleApp"
 	targetname "app"
